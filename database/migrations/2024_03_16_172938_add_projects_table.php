727eaadfd13c4431215b4cb712a596ea1cfac131
Tomislav0 <tomislav.kovacevic01@gmail.com>
2024-03-16 18:57:57 +0100

    created laravel project | migration for creating projects table

---
+++ database/migrations/2024_03_16_172938_add_projects_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id()->primary();
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->decimal('price', total: 10, places: 2);
+            $table->boolean('jobs_done')->default(false);
+            $table->date('started');
+            $table->date('finished')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::drop('projects');
+    }
+};
